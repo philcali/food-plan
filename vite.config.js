82a939e0bf54936362c91323e4ef8d61f6ffe33e
Philip Cali <philip.cali@gmail.com>
2026-07-25 16:40:44 -0400

    fix: build pathing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+const base = process.env.GITHUB_ACTIONS ? '/food-plan/' : '/';
+
 export default defineConfig({
+  base,
   plugins: [react(), tailwindcss()],
 })
